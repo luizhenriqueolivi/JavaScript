8be0c1934d9c47fb27af060dfba31c5594beee62
Luiz Henrique Olivi <98348056+luizhenriqueolivi@users.noreply.github.com>
2022-08-10 15:46:54 -0300

    Exercícios de Condição do JavaScript

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 80.
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 80) {
+    console.log('Você ultrapassou a velocidade permitida. FOI MULTADO!')    
+} 
+console.log('Dirija sempre usando o cinto!')
---
+++ ex011.js
@@ -0,0 +1,9 @@
+var idade = 66
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16) {
+    console.log('Você não pode votar')
+} else if (idade < 18 || idade > 65) {     
+        console.log('Seu voto é opcional')
+    }else {
+        console.log('Seu voto é obrigatório')
+    }

cb9660a9989752a262de07c55b4d853c7f4f9804
Luiz Henrique Olivi <98348056+luizhenriqueolivi@users.noreply.github.com>
2022-08-16 15:34:40 -0300

    Alterações no exercicio 002

---
+++ ambiente.js
@@ -0,0 +1,15 @@
+var cont = 1
+while(cont <= 6) {
+    console.log(`Passo ${cont}`)
+    cont ++  /* Significa cont = cont + 1 */
+}
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
---
+++ ex014.js
@@ -0,0 +1,4 @@
+console.log('Vai começar a contagem...')
+for (var cont =1; cont <=8; cont++){
+    console.log(cont)
+} console.log('Terminou!')
